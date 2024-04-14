cb2767d88d1e8fbff9b565472625f66a3f98d24e
NicolasGautier <nicolas.gautier35135@gmail.com>
2024-04-14 18:12:01 +0200

    javacertification update code

---
+++ src/main/java/academy/learnprogramming/DeclaringAndInitializingVariables.java
@@ -0,0 +1,44 @@
+package academy.learnprogramming;
+
+public class DeclaringAndInitializingVariables {
+
+    public static void main(String[] args) {
+
+        // declaring and initilizing in two lines
+        int myNumber;
+        myNumber = 10;
+        System.out.println("myNumber = "+myNumber);
+
+        // declaring and initilizing in one line
+        double myDouble = 7.50;
+        System.out.println("myDouble = "+myDouble);
+
+        float myFloat, myFloat2, myFloat3;
+        float myFloat4; float myFloat5;
+
+        float myFloat6 = 5f, myFloat7 = 10f, myFloat8;
+        boolean b1, b2;
+
+        // float myFloat45, float myFloat46; // doesn't compile
+
+        int i1;
+        int i2;
+        // int i3; i4; // doesn't compile
+
+        // identifiers (nom de la variable)
+        // reserved keywords
+        // int int = 10; // doesn't compile
+        char ChaR;
+        char Char;
+        double $double;
+        double _double;
+
+        // begin with number is illegal
+        // double 1double; // doesn't compile
+
+        // only digit, letter, _ or $
+        // double my@Double; // doesn't compile
+
+
+    }
+}
