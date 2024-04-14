633bab77f8888be1041af4b4db2b71b37d948057
NicolasGautier <nicolas.gautier35135@gmail.com>
2024-04-14 18:13:40 +0200

    javacertification update code

--- src/main/java/academy/learnprogramming/DefaultInitialization.java
+++ src/main/java/academy/learnprogramming/DefaultInitialization.java
@@ -1,2 +1,27 @@
-package academy.learnprogramming;public class DefaultInitialization {
+package academy.learnprogramming;
+
+public class DefaultInitialization {
+
+    static boolean myBoolean;
+    static byte myByte;
+    static short myShort;
+    static int myInt;
+    static long myLong;
+    static float myFloat;
+    static double myDouble;
+    static char myChar;
+
+    public static void main(String[] args) {
+        int localInt;
+
+        // System.out.println(localInt); // doesn't compile (no initilization)
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+        System.out.println(myShort);
+        System.out.println(myInt);
+        System.out.println(myLong);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+    }
 }
