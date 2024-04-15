2712a12818c93d257d25d30fe16ab13ca442a0a9
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-04-15 08:45:31 +0200

    JavaCertification update

--- src/main/java/academy/learnprogramming/WhileLoop.java
+++ src/main/java/academy/learnprogramming/WhileLoop.java
@@ -1,2 +1,37 @@
-package academy.learnprogramming;public class WhileLoop {
+package academy.learnprogramming;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int spaceOnHdd = 5;
+
+        while (spaceOnHdd > 0) {
+            System.out.println("spaceOnHdd " + spaceOnHdd--);
+        }
+
+        // endless loop
+        int x = 2;
+        int y = 5;
+        // while(x<10) y++;
+
+        // to fix that add x increment
+        while (x < 10) {
+            y++;
+            x++;
+        }
+        System.out.println("x " + x + "y " + y);
+
+        int a = 5;
+        int b = 7;
+
+        // a is < 6, while loop wil not execute
+        while(a>6){
+            b+=2;
+            a++;
+        }
+
+        a = a > 6 ? a++ : b--;
+        // a = 7
+        // b = 6
+        System.out.println("a " + a + "b " + b + " , " + (a+b));
+    }
 }
