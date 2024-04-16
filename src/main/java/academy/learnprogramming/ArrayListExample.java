a2ca49a9ff10c9202fce2975f45fdc047c7bb572
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-04-16 15:19:44 +0200

    JavaCertification update

--- src/main/java/academy/learnprogramming/ArrayListExample.java
+++ src/main/java/academy/learnprogramming/ArrayListExample.java
@@ -1,7 +1,20 @@
 package academy.learnprogramming;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListExample {
     public static void main(String[] args) {
 
+        ArrayList myList = new ArrayList();
+        ArrayList<Object> myList2 = new ArrayList<>();
+        ArrayList<String> myList3 = new ArrayList<String>();
+        ArrayList<String> myList4 = new ArrayList<>();
+
+        List<String> myList5 = new ArrayList<>(); // using interface as type
+
+        // ArrayList<String> list = new List<>(); // does not compile Class != interface
+
+        List<String> list2 = new ArrayList<>(20);
     }
 }
