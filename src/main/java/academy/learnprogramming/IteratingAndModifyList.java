410ce4583afd2216dab79383d9a2ddf422241197
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-04-17 10:43:49 +0200

    JavaCertification update

--- src/main/java/academy/learnprogramming/IteratingAndModifyList.java
+++ src/main/java/academy/learnprogramming/IteratingAndModifyList.java
@@ -1,35 +1,62 @@
 package academy.learnprogramming;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class IteratingAndModifyList {
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<>(Arrays.asList(createArray()));
+        List<String> list = createList();
         String str = null;
 
-        // listIterator
+        /*// listIterator
         for (ListIterator<String> listIterator = list.listIterator(); listIterator.hasNext(); str = listIterator.next()) {
 
             // remove
-            /*if (Objects.equals(str, "Array 3")) {
+            *//*if (Objects.equals(str, "Array 3")) {
                 listIterator.remove();
-            }*/
+            }*//*
 
             // modify
             if (Objects.equals(str, "Array 3")) {
                 listIterator.set("Array 3 modified");
             }
+        }*/
+
+        // for loop
+        for (int i = 0; i < list.size(); i++) {
+            String tmp = list.get(i);
+
+            // remove
+            /*if (Objects.equals(tmp, "Array 3")) {
+            list.remove(i);
+            }*/
+
+            // modify
+            if (Objects.equals(tmp, "Array 3")) {
+                list.set(i, "Array 3 modified");
+            }
         }
-        System.out.println(list);
+
+        // create a duplicate
+        list.add("Array 1");
+
+        // create another duplicate
+        list.add("Array 4");
+
+        // remove all duplicates
+        List<String> distinctedList = list.stream().distinct().toList();
+        System.out.println("list = " + list);
+        System.out.println("distinctedList = " + distinctedList);
+
     }
 
-    private static String[] createArray() {
-        String[] stringArray = new String[10];
-        for (int i = 0; i < stringArray.length; i++) {
-            stringArray[i] = "Array " + i;
+    private static  ArrayList<String> createList() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add("Array " + i);
         }
-        return stringArray;
+        return list;
     }
 
 }
