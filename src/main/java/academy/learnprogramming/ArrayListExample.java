c548dae9cdec2e78e5a1e780c21ba9ec14f6400a
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-04-18 09:28:06 +0200

    JavaCertification update

--- src/main/java/academy/learnprogramming/ArrayListExample.java
+++ src/main/java/academy/learnprogramming/ArrayListExample.java
@@ -10,11 +10,16 @@ public class ArrayListExample {
         ArrayList<Object> myList2 = new ArrayList<>();
         ArrayList<String> myList3 = new ArrayList<String>();
         ArrayList<String> myList4 = new ArrayList<>();
+        ArrayList<String> myList41 = new ArrayList<String>();
 
         List<String> myList5 = new ArrayList<>(); // using interface as type
-
-        // ArrayList<String> list = new List<>(); // does not compile Class != interface
-
         List<String> list2 = new ArrayList<>(20);
+        List list21 = new ArrayList<>();
+        List list22 = new ArrayList();
+
+        // ArrayList<> myList42 = new ArrayList<String>(); // does not compile, left operand with empty diamond operators
+        // ArrayList<String> list = new List<>(); // does not compile Class != interface
+        // List list23 = new ArrayList; // call an empty constructor at least
+
     }
 }
