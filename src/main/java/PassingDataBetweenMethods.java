bca04fcf33c9584b270f1cf0e6a7336c5573436d
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-04-18 10:15:28 +0200

    JavaCertification update

---
+++ src/main/java/PassingDataBetweenMethods.java
@@ -0,0 +1,38 @@
+public class PassingDataBetweenMethods {
+
+    public static void main(String[] args) {
+        int number = 4;
+        System.out.println("number = " + number);
+        newNumber(5);
+        System.out.println("number = " + number);
+        String name = "Jimmy";
+        System.out.println("name= " + name);
+        newName(name);
+        System.out.println("name= " + name);
+
+        StringBuilder sb = new StringBuilder();
+        StringBuilder s = sb;
+        s.append("s= " + s);
+
+        // build(sb);
+        System.out.println("s= " + s);
+        System.out.println("stringBuilder= " + sb);
+    }
+
+    public static void newNumber(int number){ // It is a copy which was passed
+        System.out.println("in new number number = " + number);
+        number = 10; // copy
+        System.out.println("in new number number = " + number);
+    }
+
+    public static void newName(String name){ // It is a copy which was passed
+        System.out.println("in new name = " + name);
+        name = "Timmy"; // copy
+        System.out.println("in new name = " + name);
+    }
+
+    public static void build(StringBuilder s){ // It is a copy of stringbuilder; these two sb have two references which point the same reference //
+        // s = new StringBuilder(); // reassign the element in string builder -> empty string
+        s.append("Tom");
+    }
+}
