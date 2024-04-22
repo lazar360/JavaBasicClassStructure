38e25f26de948dd255cd0f9c4e4972c672fe5c12
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-04-22 10:59:44 +0200

    JavaCertification update

--- src/main/java/academy/learnprogramming/ContinueStatement.java
+++ src/main/java/academy/learnprogramming/ContinueStatement.java
@@ -1,2 +1,55 @@
-package academy.learnprogramming;public class ContinueStatement {
+package academy.learnprogramming;
+
+public class ContinueStatement {
+    public static void main(String[] args) {
+        String[] animals = {"Dog", "Cat", "Lizard", "Bird", "Snake"};
+
+        /*for (String animal : animals) {
+            System.out.println(animal);
+        }*/
+
+        MY_LOOP : for (String animal : animals) {
+            if(animal.equals("Cat")){
+                continue MY_LOOP;
+            }
+            System.out.println(animal);
+        }
+        System.out.println();
+
+        animal : for (String animal : animals) {
+            if(animal.equals("Cat")){
+                continue animal;
+            }
+            System.out.println(animal);
+        }
+
+        System.out.println();
+        for (String animal : animals) {
+            if(animal.equals("Lizard")){
+                continue;
+            }
+            System.out.println(animal);
+        }
+        System.out.println();
+
+        for (String animal : animals) {
+            if(animal.equals("Lizard") || animal.equals("Cat")){
+                continue;
+            }
+            System.out.println(animal);
+        }
+        System.out.println();
+
+        int index = 0;
+        while(index < animals.length-1){
+            String animal = animals[index];
+            index++; // if not, endless loop or outofboundexception
+
+            if(animal.equals("Lizard")){
+                continue;
+            }
+            System.out.println(animal);
+        }
+
+    }
 }
---
+++ src/main/java/inheritance/Main/Polymorphism.java
@@ -0,0 +1,2 @@
+package inheritance.Main;public class Polymorphism {
+}
