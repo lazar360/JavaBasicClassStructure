ad633ab8c664020b38f5ffea269996770f388333
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-04-23 08:20:58 +0200

    JavaCertification update

--- src/main/java/academy/learnprogramming/Counter.java
+++ src/main/java/academy/learnprogramming/Counter.java
@@ -1,2 +1,29 @@
-package academy.learnprogramming;public class Counter {
+package academy.learnprogramming;
+
+public class Counter {
+    public static int count;
+    public int anotherCount;
+
+    public Counter(){
+        count++;
+        anotherCount++;
+    }
+
+    public static void main(String[] args) {
+        Counter one = new Counter();
+        Counter two = new Counter();
+        Counter three = new Counter();
+        Counter four = new Counter();
+
+        System.out.println(count); // 4
+        System.out.println(one.count); // 4
+        System.out.println(two.count); // 4
+        System.out.println(three.count); // 4
+        System.out.println(four.count); // 4
+
+        System.out.println(one.anotherCount); // 1
+        System.out.println(two.anotherCount); // 1
+        System.out.println(three.anotherCount); // 1
+        System.out.println(four.anotherCount); // 1
+    }
 }
