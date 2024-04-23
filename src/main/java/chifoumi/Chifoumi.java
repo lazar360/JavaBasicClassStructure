7358d7dcf93290bbeaf4966ae324efb6d26024f8
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-04-23 12:29:46 +0200

    JavaCertification update

--- src/main/java/chifoumi/Chifoumi.java
+++ src/main/java/chifoumi/Chifoumi.java
@@ -1,6 +1,8 @@
 package chifoumi;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class Chifoumi {
@@ -19,10 +21,20 @@ public class Chifoumi {
 
     public static void main(String[] args) {
 
-        System.out.println("résultat= " + determineResult(userChoice, computerChoice));
-
+        String result = determineResult(userChoice, computerChoice);
+        String translation = translateResult(result);
+        System.out.println("résultat= " + result);
+        System.out.println("traduction= " + translation);
         }
-        public static String determineResult(String userChoice, String computerChoice){
+
+    private static String translateResult(String result) {
+        Map<String, String> translateMap = new HashMap<>();
+        translateMap.put("égalité", "rejoue encore");
+        translateMap.put("utilisateur gagne", "ça veut dire que tu peux te casser maintenant");
+        return Objects.equals(result, " ") ? "je ne sais pas" : translateMap.get(result);
+    }
+
+    public static String determineResult(String userChoice, String computerChoice){
             for (String[] res : result) {
                 if (res[0].equals(userChoice) && res[1].equals(computerChoice)) {
                     return res[2];
