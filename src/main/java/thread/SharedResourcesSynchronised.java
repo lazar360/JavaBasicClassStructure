75884840d477d77727b83544c59066c1c258f093
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-04-25 10:23:52 +0200

    JavaCertification update

--- src/main/java/thread/SharedResourcesSynchronised.java
+++ src/main/java/thread/SharedResourcesSynchronised.java
@@ -1,11 +1,15 @@
 package thread;
 
-class A extends Thread{
-    public static int content;
+class B extends Thread{
+
+    public static int content2;
+    static synchronized void increment(){
+        content2++; // partie critique (lock this block of code with synchronised)
+    }
 
     public void run(){
         for (int i = 0; i < 10; i++) {
-        content++; // partie critique (lock this block of code)
+            increment();
             try {
                 sleep(100);
             } catch (InterruptedException e) {
@@ -14,16 +18,16 @@ class A extends Thread{
         }
     }
 }
-public class SharedResources {
+public class SharedResourcesSynchronised {
     public static void main(String[] args) {
-        A a1 = new A();
-        A a2 = new A();
-        a1.start();
-        a2.start();
+        B b1 = new B();
+        B b2 = new B();
+        b1.start();
+        b2.start();
         try {
-            a1.join();
-            a2.join();
+            b1.join();
+            b2.join();
         } catch (Exception ignored){}
-        System.out.println("content= " + A.content); // normalement 20
+        System.out.println("content= " + B.content2); // normalement 20
     }
 }
