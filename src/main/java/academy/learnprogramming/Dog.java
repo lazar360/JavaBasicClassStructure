a8102fe795cff62de8c4a807ed0f7f22d7260a2a
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-04-25 08:39:29 +0200

    JavaCertification update

--- src/main/java/academy/learnprogramming/Dog.java
+++ src/main/java/academy/learnprogramming/Dog.java
@@ -1,2 +1,9 @@
-package academy.learnprogramming;public class Dog {
+package academy.learnprogramming;
+
+public class Dog {
+
+    public static int count = 0;
+    public static void incrementCounter(){
+        count++;
+    }
 }
