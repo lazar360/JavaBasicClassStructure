694ea7e9606ddf345633b74e2947cd1c6c22c5c6
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-04-25 09:51:10 +0200

    JavaCertification update

--- src/main/java/thread/ThreadExample.java
+++ src/main/java/thread/ThreadExample.java
@@ -1,2 +1,55 @@
-package thread;public class ThreadExample {
+package thread;
+
+import java.util.Random;
+
+public class ThreadExample {
+
+    static class Request extends Thread {
+        String[] request = {"vacation request", "rtt request", "improve remuneration"};
+
+        public void TransmitRequest() {
+            for (int i = 0; i < 3; i++) {
+                System.out.println(request[new Random().nextInt(0,3)]);
+            }
+        }
+
+        public void run(){
+            TransmitRequest();
+        }
+    }
+
+    static class InfoForRequest extends Thread {
+        String[] info = {"need to go to Bahamas", "my aunt is dead", "I work hard every day"};
+
+        public void TransmitInfo() {
+            for (int i = 0; i < 3; i++) {
+                System.out.println(info[new Random().nextInt(0,3)]);
+            }
+        }
+
+        public void run(){
+            TransmitInfo();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Request theRequest = new Request();
+        InfoForRequest theInfoForRequest = new InfoForRequest();
+        // Start the run methods
+        theRequest.start();
+        theInfoForRequest.start();
+
+        // Define a priority level
+        theRequest.setPriority(Thread.MAX_PRIORITY);
+        theInfoForRequest.setPriority(Thread.MIN_PRIORITY);
+
+        // Finish run methods before System.out.println("Your boss says : Another thing ?");
+        try {
+        theInfoForRequest.join();
+        theRequest.join();
+        } catch (Exception ignored){}
+        System.out.println("Your boss says : Another thing ?");
+
+    }
 }
