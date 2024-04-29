bad2cb59c77bc7c27c87c9a0af91dd59b019b061
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-04-29 08:24:12 +0200

    JavaCertification update

--- src/main/java/academy/learnprogramming/DogTester.java
+++ src/main/java/academy/learnprogramming/DogTester.java
@@ -1,2 +1,30 @@
-package academy.learnprogramming;public class DogTester {
+package academy.learnprogramming;
+
+public class DogTester {
+    public static void main(String[] args) {
+
+        Dog husky = new Dog();
+        System.out.println(husky.count);
+        System.out.println(Dog.count);
+
+        Dog anotherDog = new Dog();
+        anotherDog.incrementCounter();
+        Dog.incrementCounter();
+        System.out.println(Dog.count);
+
+        husky = null;
+        anotherDog = null;
+
+        System.out.println(Dog.count);
+        System.out.println(husky.count);
+        System.out.println(anotherDog.count);
+
+        Dog.count = 10;
+        Dog newDog = new Dog();
+        Dog dog2 = new Dog();
+        newDog.count = 5;
+        newDog.count = 11;
+
+        System.out.println(Dog.count); //11
+    }
 }
