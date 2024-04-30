8dea5dace5d9ac36fd943a39a0a069872809fd2b
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-04-30 09:01:36 +0200

    JavaCertification update

--- src/main/java/academy/learnprogramming/FinalVariables.java
+++ src/main/java/academy/learnprogramming/FinalVariables.java
@@ -1,2 +1,30 @@
-package academy.learnprogramming;public class FinalVariables {
+package academy.learnprogramming;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FinalVariables {
+
+    private static final int SIZE = 10;
+    // private static int SIZE = 10; // throws an out of bound exception in the for loop
+    private static final List<String> VALUES = new ArrayList<>();
+
+    public static void main(String[] args) {
+        int[] myArray = new int[SIZE];
+
+        // SIZE = 11;
+        // SIZE++;
+        int doubleSize = 2 * SIZE;
+
+        for (int i = 0; i < SIZE; i++) {
+            System.out.println(myArray[i]);
+        }
+
+        VALUES.add("changed"); // we can add an element to a final list
+        // VALUES = new ArrayList<>(); // we can not reassign a reference to the list
+        final int myNumber = 10;
+        // myNumber = 11; does not compile
+
+    }
 }
