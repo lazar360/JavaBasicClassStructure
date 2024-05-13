cebe8673f524bfdfaefd5d324bc10d02aac1edf2
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-05-13 08:26:39 +0200

    JavaCertification update

--- src/main/java/academy/learnprogramming/IfElseStatement.java
+++ src/main/java/academy/learnprogramming/IfElseStatement.java
@@ -1,2 +1,24 @@
-package academy.learnprogramming;public class IfElseStatement {
+package academy.learnprogramming;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+        boolean b = false;
+        if (!b){
+            System.out.println("false");
+        } else {
+            System.out.println("true");
+        }
+
+        int x = 1;
+        //if(x){} does not compile
+        //if(x=1){} does not compile
+
+        //-----------
+        int hoursOfDay = 10;
+        if(hoursOfDay<11) System.out.println("Morning");
+        else if(hoursOfDay<17) System.out.println("Evening");
+        else
+            System.out.println("Day");
+
+    }
 }
