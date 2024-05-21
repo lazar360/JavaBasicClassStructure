bb016f0cf96576e36bf4e47010d3d0f2d025777b
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-05-21 08:33:04 +0200

    update code

--- src/main/java/academy/learnprogramming/MethodArguments.java
+++ src/main/java/academy/learnprogramming/MethodArguments.java
@@ -1,2 +1,31 @@
-package academy.learnprogramming;public class MethodArguments {
+package academy.learnprogramming;
+
+public class MethodArguments {
+
+    int getZEro() {
+        return 0;
+    }// 0 arguments niladic
+
+    int increment(int i) {
+        return i + 1;
+    }// 1 arguments monadic
+
+    int sum(int i, int j) {
+        return i + j;
+    }// 2 arguments dyadic
+
+    int plusMinus(int i, int j, int k) {
+        return i + j - k;
+    }// 3 arguments (triadic) -> to avoid
+
+    double average(double a, double b, double c, double d) {
+        return a + b + c + d;
+    } // 4 arguments or + (polyadic) -> to avoid
+
+    // void test {} // does not compile no brackets
+
+    // void test(int a; int b){} // does not compile no brackets
+    // void test(); // does not compile missing method body
+
+
 }
