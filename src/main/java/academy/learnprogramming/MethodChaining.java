a109f1c1bffe85ff9c092d459f406a448d51a804
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-05-22 08:54:43 +0200

    update code

--- src/main/java/academy/learnprogramming/MethodChaining.java
+++ src/main/java/academy/learnprogramming/MethodChaining.java
@@ -1,2 +1,46 @@
-package academy.learnprogramming;public class MethodChaining {
+package academy.learnprogramming;
+
+import java.util.Objects;
+
+public class MethodChaining {
+
+    public static void main(String[] args) {
+        String start = "   Java  ";
+        String trimmed = start.trim();
+        System.out.println("trimmed = " + trimmed);
+
+        String toLowerCase = trimmed.toLowerCase();
+        System.out.println("lowercase = " + toLowerCase);
+
+        String result = toLowerCase.replace('j', 'J');
+        System.out.println("result = " + result);
+
+        String anotherResult = "   Java  ".trim().toLowerCase().replace('j', 'J');
+        System.out.println("anotherResult = " + anotherResult);
+        System.out.println("anotherResult == result " + Objects.equals(result, anotherResult));
+
+        //-----------
+        String a = "abc";
+        String b = a.toUpperCase(); // produces a new String, does not change a variable
+        String c = b.replace('B', 'b').replace('C', 'c');// produces new String
+        // a= abc
+        // b= ABC
+        // c= Abc
+        System.out.println("a= " + a);
+        System.out.println("b= " + b);
+        System.out.println("c= " + c);
+
+        if(a.equalsIgnoreCase(b)){
+            System.out.println("Strings are equal");
+        }
+
+        if(a.toLowerCase().trim().equals(b.toLowerCase().trim())){
+            System.out.println("Equal");
+        }
+        // Strings are equal
+        // Equal
+        //-----------
+
+
+    }
 }
