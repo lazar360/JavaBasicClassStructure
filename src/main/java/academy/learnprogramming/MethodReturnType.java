6260ea0ce53191b4fe506d989987256909246c66
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-05-24 09:23:11 +0200

    update code

--- src/main/java/academy/learnprogramming/MethodReturnType.java
+++ src/main/java/academy/learnprogramming/MethodReturnType.java
@@ -1,2 +1,49 @@
-package academy.learnprogramming;public class MethodReturnType {
+package academy.learnprogramming;
+
+public class MethodReturnType {
+
+    public void jump() {
+    } // return type void
+
+    public void jump2() {
+        return;
+    }
+
+    public String jump3() {
+        return "";
+    }
+
+    // public String jump4(){return;} // does not compile because no return statement
+    // public jump5(){} // does not compile, no return type specified
+    /* Does not compile
+    String jump6(int a){
+        if(a==0)return "";
+    }*/
+    String jump6(int a) {
+        if (a == 0) return "";
+        return "abc";
+    }
+
+    int getInt(){
+        return 9;
+    }
+
+    int getLong(){
+        return (int) 9L;
+    }
+
+    int expanded(){
+        // int temp = 10;
+        // return temp;
+        return 10;
+    }
+
+    boolean isTrue(){
+        return 5 < 10;
+    }
+
+    // public void 2test(){} // does not compile
+    // public void void(){} // does not compile
+    public void $test(){}
+    public void _test(){}
 }
