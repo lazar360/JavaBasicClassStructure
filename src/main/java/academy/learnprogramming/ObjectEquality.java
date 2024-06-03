995f5bf51218ea2e6c6fe1c7af8d4ff34fd94d14
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-06-03 08:24:27 +0200

    update code

--- src/main/java/academy/learnprogramming/ObjectEquality.java
+++ src/main/java/academy/learnprogramming/ObjectEquality.java
@@ -1,2 +1,33 @@
-package academy.learnprogramming;public class ObjectEquality {
+package academy.learnprogramming;
+
+import java.util.Objects;
+
+public class ObjectEquality {
+    public static void main(String[] args) {
+        // comparing objects
+        /*Integer myInteger = 127;
+        Integer myOtherInteger = 127;
+        System.out.println(myInteger == myOtherInteger); // true car inférieur à 128 (utilisation du cache)
+
+        myInteger = 128;
+        myOtherInteger = 128;
+        System.out.println(myInteger == myOtherInteger); // false*/
+
+        Integer myInteger = 128;
+        Integer myOtherInteger = 128;
+        System.out.println(myInteger == myOtherInteger);
+
+        Integer int1 = new Integer(1);
+        Integer int2 = new Integer(1);
+        Integer int3 = 1;
+        System.out.println(int1 == int2);
+        System.out.println(int1 == int3);
+        System.out.println(int2 == int3);
+
+        int2 = null;
+        System.out.println("Objects.equals(int1, int2) : " + Objects.equals(int1, int2)); // false
+        System.out.println(int1.equals(int3));
+        System.out.println(int2.equals(int3)); // throw a null pointer exception
+
+    }
 }
