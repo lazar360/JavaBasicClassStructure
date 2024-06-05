e9a0ca9c7b89562bd2c0cfed8a6f9f3468cfc28a
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-06-05 08:58:54 +0200

    update code

--- src/main/java/academy/learnprogramming/OrderOfInitialization.java
+++ src/main/java/academy/learnprogramming/OrderOfInitialization.java
@@ -1,2 +1,39 @@
-package academy.learnprogramming;public class OrderOfInitialization {
+package academy.learnprogramming;
+
+class Demo {
+
+    static { // static 1
+        add(2);
+    }
+
+    static void add(int number) {
+        System.out.print(number + " ");
+    }
+
+    Demo() {
+        add(5);
+    } // constructor 6 and 9
+
+    static { // static 2
+        add(4);
+    }
+
+    {
+        add(6); // instance initializer 4 and 7
+    }
+
+    static { // static 3
+        new Demo(); // call instance initializer
+    }
+
+    {
+        add(8); // instance initializer 5 and 8
+    }
+}
+
+public class OrderOfInitialization {
+
+    public static void main(String[] args) {
+        new Demo(); // call instance initializer and constructor
+    }
 }
