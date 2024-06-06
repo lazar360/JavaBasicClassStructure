3b423eacb38ff548337706d60e9b51cadeb2ca8a
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-06-06 10:08:14 +0200

    update code

--- src/main/java/academy/learnprogramming/OverloadingMethods.java
+++ src/main/java/academy/learnprogramming/OverloadingMethods.java
@@ -1,2 +1,40 @@
-package academy.learnprogramming;public class OverloadingMethods {
+package academy.learnprogramming;
+public class OverloadingMethods {
+
+    public void walk(int miles){}
+    public void walk(short feet){}
+    public boolean walk(){
+        return false;
+    }
+
+    void walk(int miles, short feet){}
+    public void walk(float miles){}
+    // public int walk(float km){} it does not compile
+    public void walk(int[] length){}
+    // public void walk(int... vars){} does not compile because same parameter type (int[] length = int... vars)
+
+    public static void main(String[] args) {
+
+        run(1);// call run(int length)
+        run(1.0);// call run(double length)
+        run(1L); // call run(double length)
+
+        jump("test"); //
+        jump(25);
+
+
+    }
+
+    public static void run(int length){}
+    public static void run(Integer length){}
+    public static void run(double length){}
+
+    public static void jump(String s){
+        System.out.println("string");
+    }
+
+    public static void jump(Object o){
+        System.out.println("Object");
+    }
+
 }
