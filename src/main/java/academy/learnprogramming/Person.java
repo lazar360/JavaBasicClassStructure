2d7ac4234b4b866b6e43bbe3de86a2eb26479875
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-06-10 08:57:47 +0200

    update code

--- src/main/java/academy/learnprogramming/Person.java
+++ src/main/java/academy/learnprogramming/Person.java
@@ -6,12 +6,24 @@ public class Person{
     private String lastName;
     private int age;
 
+    public Person(){
+        this("Eric", "Johnson");
+    }
+    public Person(String firstName, String last){
+        this.firstName = firstName;
+        lastName = last; // this is optional
+    }
+
     public String getFirstName(){
         return firstName;
     }
 
     public void setFirstName(String firstName){
         this.firstName = firstName;
+    }
+
+    public String getFullName(){
+        return firstName +" "+ lastName;
     }
 
         }
