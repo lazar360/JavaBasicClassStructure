c8cc35759e8629ad0dfba68dc135a17681ae71b9
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-06-11 08:44:02 +0200

    update code

--- src/main/java/academy/learnprogramming/RelationalOperators.java
+++ src/main/java/academy/learnprogramming/RelationalOperators.java
@@ -1,2 +1,14 @@
-package academy.learnprogramming;public class RelationalOperators {
+package academy.learnprogramming;
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z = 10;
+
+        System.out.println("x < y : " + (x < y));
+        System.out.println("x <= y :" + (x <= y));
+        System.out.println("x >= z :" + (x >= z));
+        System.out.println("x > z :" + (x > z));
+    }
 }
