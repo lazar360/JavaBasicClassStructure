cf23dcb17184a99452ef6c252c8af20eb8c961e9
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-06-14 08:54:32 +0200

    update code

--- src/main/java/academy/learnprogramming/ReturningDataFromMethods.java
+++ src/main/java/academy/learnprogramming/ReturningDataFromMethods.java
@@ -1,2 +1,22 @@
-package academy.learnprogramming;public class ReturningDataFromMethods {
+package academy.learnprogramming;
+
+public class ReturningDataFromMethods {
+    public static void main(String[] args) {
+        int number = 2;
+        String word = "xyz";
+        number(number); // number is not assigned, so no changes
+        // number = number(number);
+
+        word = word(word);
+        System.out.println(number + word);
+    }
+
+    public static int number(int number){
+        number++;
+        return number;
+    }
+    public static  String word(String word){
+        word += "a";
+        return word;
+    }
 }
