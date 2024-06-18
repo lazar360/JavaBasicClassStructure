408871523ec9f024ff06f1b52d0a9c2ffd817a9b
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-06-18 08:50:46 +0200

    update code

--- src/main/java/academy/learnprogramming/SearchingList.java
+++ src/main/java/academy/learnprogramming/SearchingList.java
@@ -1,2 +1,41 @@
-package academy.learnprogramming;public class SearchingList {
+package academy.learnprogramming;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SearchingList {
+    public static void main(String[] args) {
+        List<String> numsList = new ArrayList<>();
+        numsList.add("500");
+        numsList.add("10");
+        numsList.add("100000000");
+        numsList.add("9");
+        numsList.add("900");
+        numsList.add("50");
+        numsList.add("40");
+        numsList.add("700");
+
+        System.out.println(numsList);
+        int index = Collections.binarySearch(numsList, "10");
+        System.out.println(index);
+
+        Collections.sort(numsList);
+        System.out.println(numsList);
+        index = Collections.binarySearch(numsList, "10");
+        System.out.println(index);
+
+        List<Integer> numbers = Arrays.asList(5,4,8,10,11,7,3);
+        System.out.println(numbers);
+
+        index = Collections.binarySearch(numbers,7);
+        System.out.println(index);
+
+        Collections.sort(numbers);
+        System.out.println(numbers);
+
+        index = Collections.binarySearch(numbers, 7);
+        System.out.println(index);
+    }
 }
