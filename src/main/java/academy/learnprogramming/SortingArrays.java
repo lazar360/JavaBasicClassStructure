3bcd39002eb960855f08b9c79a068681f51aa2b1
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-06-19 08:20:44 +0200

    update code

--- src/main/java/academy/learnprogramming/SortingArrays.java
+++ src/main/java/academy/learnprogramming/SortingArrays.java
@@ -1,2 +1,18 @@
-package academy.learnprogramming;public class SortingArrays {
+package academy.learnprogramming;
+
+import java.util.Arrays;
+
+public class SortingArrays {
+    public static void main(String[] args) {
+        int[] numbers = {5,10,2};
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+        System.out.println();
+
+        String[] strings = {"5","500","9"};
+        Arrays.sort(strings); // sort in alphabetical order, five begins with b and nine with n, so 5,500,9
+        System.out.println(Arrays.toString(strings));
+        System.out.println();
+
+    }
 }
