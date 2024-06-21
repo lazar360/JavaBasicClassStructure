fdf51ddb327097d4b3753936d86605697fd9c5e8
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-06-21 08:37:51 +0200

    update code

--- src/main/java/academy/learnprogramming/Static.java
+++ src/main/java/academy/learnprogramming/Static.java
@@ -1,2 +1,33 @@
-package academy.learnprogramming;public class Static {
+package academy.learnprogramming;
+
+public class Static {
+
+    private String name = "Static";
+
+    public static void one(){}
+    public static void two(){}
+    public static void three(){
+        one();
+        two();
+        // four(); does not compile
+    }
+
+    // public static void four(){ // name is not in static context
+    public void four(){
+        one();
+        two();
+        three();
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        Static.one();
+        Static.two();
+        Static.three();
+        // Static.four(); does not compile, four is not in static context
+        // Must have an instance of the class
+        Static myInstance = new Static();
+        myInstance.four();
+        new Static().four();
+    }
 }
