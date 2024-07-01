751a7e7dc37129674a29bd3f7936ea2600675b25
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-07-01 08:04:56 +0200

    update code

--- src/main/java/academy/learnprogramming/StringImmutability.java
+++ src/main/java/academy/learnprogramming/StringImmutability.java
@@ -1,2 +1,22 @@
-package academy.learnprogramming;public class StringImmutability {
+package academy.learnprogramming;
+
+public class StringImmutability {
+    public static void main(String[] args) {
+        String hello = "hello";
+        String hi = hello + "world";
+        hi = hello;
+        System.out.println(hi+hello);
+
+        hello = hello.toUpperCase();
+
+        System.out.println(hello); // HELLO
+
+        // concat
+        String s1 = "1";
+        String s2 = s1.concat("2");
+        s2.concat("3"); // a String is immutable (concat generate a new String)
+        System.out.println(s1);//1
+        System.out.println(s2);//12
+
+    }
 }
