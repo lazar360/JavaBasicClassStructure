d42ddbe257075ee0608e9d4f61f9339a8ecc4d7b
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-07-03 12:32:45 +0200

    update code

--- src/main/java/academy/learnprogramming/StringMethods2.java
+++ src/main/java/academy/learnprogramming/StringMethods2.java
@@ -1,2 +1,38 @@
-package academy.learnprogramming;public class StringMethods2 {
+package academy.learnprogramming;
+
+public class StringMethods2 {
+    public static void main(String[] args) {
+        String str = "java";
+        String str2 = "Java";
+        String str3 = "ja";
+
+        System.out.println(str.equals(str2)); // false
+        System.out.println(str.equalsIgnoreCase(str2)); // true
+
+        // startsWith / endsWith
+        System.out.println(str.startsWith("j"));
+        System.out.println(str.startsWith(str3));
+        System.out.println(str.startsWith("J"));
+        System.out.println(str.startsWith("J".toLowerCase()));
+        System.out.println(str.endsWith("va"));
+
+        // contains
+        System.out.println(str.contains(str3));
+        System.out.println(str.contains("av"));
+        System.out.println("Java".contains("j"));
+        System.out.println("Java".contains("j".toUpperCase()));
+
+        // replace
+        String myString = "Java" + " " + "is" + " " + "cool";
+        System.out.println(myString.replace('a', 'A'));
+        System.out.println(myString.replace(" ", "_"));
+        System.out.println(myString.replace("is", "_"));
+
+        // trim
+        System.out.println("Java".trim());
+        System.out.println(" Java is cool   ");
+        System.out.println(" Java is cool   ".trim());
+
+
+    }
 }
