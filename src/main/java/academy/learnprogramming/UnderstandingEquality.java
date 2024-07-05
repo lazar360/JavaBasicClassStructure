b3f43446dcb365374ccb9c0f658b07b98986eccb
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-07-05 08:53:40 +0200

    update code

--- src/main/java/academy/learnprogramming/UnderstandingEquality.java
+++ src/main/java/academy/learnprogramming/UnderstandingEquality.java
@@ -1,2 +1,27 @@
-package academy.learnprogramming;public class UnderstandingEquality {
+package academy.learnprogramming;
+
+public class UnderstandingEquality {
+    public static void main(String[] args) {
+        StringBuilder one = new StringBuilder();
+        StringBuilder two = new StringBuilder();
+        StringBuilder three = one.append("Java"); // assign the same reference - same as Stringbuilder three = one
+
+        System.out.println(one == two); // false
+        System.out.println(one == three); // true
+
+        String x = "Java";
+        String y = "Java";
+
+        System.out.println(x == y); // true : put in String pool
+
+        String a = "Java";
+        String b = " Java".trim(); // return a new String
+
+        System.out.println(a == b); // false
+
+        String c = "Java";
+        String d = " Ja".trim()+"va"; // return a new instance of String
+
+        System.out.println(c == d);
+    }
 }
