3760de65d7473980f2090e69f985c270ee307e21
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-07-29 09:33:18 +0200

    update code

--- src/main/java/academy/learnprogramming/UsingAccessModifier.java
+++ src/main/java/academy/learnprogramming/UsingAccessModifier.java
@@ -1,2 +1,18 @@
-package academy.learnprogramming;public class UsingAccessModifier {
+package academy.learnprogramming;
+
+import academy.learnprogramming.common.Common;
+
+public class UsingAccessModifier {
+    public static void main(String[] args) {
+        Common common = new Common();
+        common.publicPrint();
+        // common.protectedPrint();
+        // common.defaultPrint();
+        // common.privatePrint(); // does not compile
+
+        System.out.println("publicNumber= " + common.publicNumber);
+        /*System.out.println("protectedNumber= " + common.protectedNumber);
+        System.out.println("defaultNumber= " + common.defaultNumber);
+        System.out.println("privateNumber= " + common.privateNumber);*/
+    }
 }
