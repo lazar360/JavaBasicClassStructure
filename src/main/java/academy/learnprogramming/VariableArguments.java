f008e47419a5a1e7c935c4f58bd89ec25a9fa913
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-08-07 08:28:22 +0200

    update code

--- src/main/java/academy/learnprogramming/VariableArguments.java
+++ src/main/java/academy/learnprogramming/VariableArguments.java
@@ -1,2 +1,20 @@
-package academy.learnprogramming;public class VariableArguments {
+package academy.learnprogramming;
+
+import java.util.Arrays;
+
+public class VariableArguments {
+    /*   public static void main(String[] args) {
+
+    }*/
+
+    public static void main(String... args) {
+        System.out.println(args.length);
+        print(args);
+        print("Java", "Java","Java","Java");
+        print("Java", "is","very","Nice");
+    }
+
+    private static void print(String... myArgs) {
+        System.out.println(Arrays.toString(myArgs));
+    }
 }
