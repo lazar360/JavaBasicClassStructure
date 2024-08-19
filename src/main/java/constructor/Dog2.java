7f4857b741dfc1c4beb61ad87b4dec6c87c6ffcb
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-08-19 08:32:08 +0200

    update code

--- src/main/java/constructor/Dog2.java
+++ src/main/java/constructor/Dog2.java
@@ -1,4 +1,6 @@
-public class Dog {
+package constructor;
+
+public class Dog2 {
     // private Dog(){}
     // public Dog(){}
     // protected Dog(){}
@@ -8,23 +10,28 @@ public class Dog {
     private String breed;
     private double weight;
 
-    public Dog(){}
-    public Dog (String name){
-        this.name = name;
+    public Dog2(){}
+    public Dog2(String name){
+        /*this.name = name;
         breed = "husky";
-        weight = 30.0;
+        weight = 30.0;*/
+        this(name, "husky"); // must be first
+        System.out.println("constructor 1");
     }
 
-    public Dog (String name, String breed){
-        this.name = name;
+    public Dog2(String name, String breed){
+        /*this.name = name;
         this.breed = breed;
-        weight = 30.0;
+        weight = 30.0;*/
+        this(name, breed, 30.0);
+        System.out.println("constructor 2");
     }
 
-    public Dog (String name, String breed, double weight){
+    public Dog2(String name, String breed, double weight){
         this.name = name;
         this.breed = breed;
         this.weight = weight;
+        System.out.println("constructor 3");
     }
 
     public void print(){
