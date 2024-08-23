3cb53e075f6e2c83d90ccf28f6d2762b7a4de583
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-08-23 08:33:08 +0200

    update code

--- src/main/java/datetime/ParsingDateTime.java
+++ src/main/java/datetime/ParsingDateTime.java
@@ -1,2 +1,31 @@
-package datetime;public class ParsingDateTime {
+package datetime;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+
+public class ParsingDateTime {
+    public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM dd yyyy");
+        LocalDate date = LocalDate.parse("02 15 2010", formatter);
+        LocalTime time = LocalTime.parse("11:33");
+        System.out.println(date);
+        System.out.println(time);
+
+        String text =
+                date.format(formatter);
+        LocalDate parsedDate =
+                LocalDate.parse(text, formatter);
+        System.out.println(text);
+        System.out.println(parsedDate);
+
+        LocalDate myDate = LocalDate.of(2010, Month.MAY, 5);
+        String dateString = formatter.format(myDate);
+        System.out.println(myDate);
+
+        LocalDate anotherDate = LocalDate.parse(dateString, formatter);
+        anotherDate.plusMonths(5).plusDays(10);
+        System.out.println(anotherDate); // anotherDate is not reassigned
+    }
 }
