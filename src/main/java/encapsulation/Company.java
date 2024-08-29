3b649ae27e485b509eeddc12c9cc1992ab98bb24
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-08-29 10:00:52 +0200

    update code

--- src/main/java/encapsulation/Company.java
+++ src/main/java/encapsulation/Company.java
@@ -1,4 +1,4 @@
-package academy.learnprogramming;
+package encapsulation;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -6,11 +6,33 @@ import java.util.List;
 
 public class Company {
 
-    public String name;
-    public List<String> employees = new ArrayList<>();
+    private String name;
+
+    private List<String> employees = new ArrayList<>();
 
     public void printSorted() {
         System.out.println("companyName= " + name);
         Collections.sort(employees);
+        System.out.println(employees);
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getEmployees() {
+        return employees;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? "null" : name;
+    }
+
+   public void addEmployee(String name){
+        if(name == null){
+            System.out.println("Can not add a null employee");
+            return;
+        }
+        employees.add(name);
+   }
 }
