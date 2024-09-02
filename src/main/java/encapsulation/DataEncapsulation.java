3b7954aedaf834118e0eab319aa1041d88f1cde1
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-09-02 09:08:11 +0200

    update code

--- src/main/java/encapsulation/DataEncapsulation.java
+++ src/main/java/encapsulation/DataEncapsulation.java
@@ -1,2 +1,19 @@
-package encapsulation;public class DataEncapsulation {
+package encapsulation;
+
+import java.util.List;
+
+public class DataEncapsulation {
+    public static void main(String[] args) {
+        Company company = new Company();
+        company.setName("My company");
+        company.addEmployee("John");
+        company.addEmployee("Anthony");
+        company.addEmployee(null);
+        company.setName(null);
+        company.printSorted();
+        /*company.name = null;
+        company.printSorted();*/
+        /*company.employees = null;
+        company.printSorted();*/
+    }
 }
