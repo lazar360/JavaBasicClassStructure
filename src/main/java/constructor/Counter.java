4ca52ac1d5c050c593d382617a9a6b082df5b8a4
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-09-12 08:57:43 +0200

    update code

--- src/main/java/constructor/Counter.java
+++ src/main/java/constructor/Counter.java
@@ -1,2 +1,21 @@
-package constructor;public class Counter {
+package constructor;
+
+public class Counter {
+
+    int count;
+    private static void increment(Counter counter){
+        counter.count++; // counter.count = counter.count + 1;
+    }
+
+    public static void main(String[] args) {
+        Counter c1 = new Counter();
+        Counter c2 = c1;
+        Counter c3 = null;
+        c2.count = 1000;
+        System.out.println("c1.count= " + c1.count); // 1000
+        System.out.println("c2.count= " + c2.count); // 1000
+        increment(c2);
+        System.out.println("c1.count= " + c1.count); // 1001
+        System.out.println("c2.count= " + c2.count); // 1001
+    }
 }
