996ad3dbd43d9ff1279a969a863949640be6dadd
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-09-24 09:43:48 +0200

    update code

--- src/main/java/exception/CatchingExceptions.java
+++ src/main/java/exception/CatchingExceptions.java
@@ -1,2 +1,31 @@
-package exception;public class CatchingExceptions {
+package exception;
+
+public class CatchingExceptions {
+
+    static class MuseumClosed extends RuntimeException{
+
+    }
+
+    static class MuseumClosedForLunch extends MuseumClosed{
+
+    }
+    public static void main(String[] args) {
+
+        try {
+            visitMuseum();
+        }catch (MuseumClosedForLunch mc){ // first subclass
+            System.out.println("Closed for lunch");
+        }catch (MuseumClosed mc){ // second super class
+            System.out.println("Closed");
+        }
+    }
+
+    public static void visitMuseum(){
+        boolean b = Math.random() < 0.5;
+
+        if(b){
+            throw new MuseumClosed();
+        }
+        throw new MuseumClosedForLunch();
+    }
 }
