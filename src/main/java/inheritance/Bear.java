529f23becaa4229cd0ef8211636373269c501b8f
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-11-08 09:07:32 +0100

    Update gitignore

--- src/main/java/inheritance/Bear.java
+++ src/main/java/inheritance/Bear.java
@@ -1,2 +1,27 @@
-package inheritance;public class Bear {
+package inheritance;
+
+public class Bear extends Animal implements Omnivore{
+    public Bear(int age) {
+        super(age);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Bear runs");
+    }
+
+    @Override
+    protected void printName() {
+
+    }
+
+    @Override
+    public void eatMeat() {
+        System.out.println("Bear eating meat");
+    }
+
+   /* @Override
+    public void eatPlants() {
+        System.out.println("Bear eating plants");
+    }*/
 }
--- src/main/java/inheritance/Bird.java
+++ src/main/java/inheritance/Bird.java
@@ -1,2 +1,16 @@
-package inheritance;public class Bird {
+package inheritance;
+
+public class Bird {
+
+    public void fly(){
+        System.out.println("Bird is flying");
+    }
+
+    public void eat(int amount){
+        System.out.println("Bird is eating " + amount + " units of food");
+    }
+
+    protected void sayHello(){
+        System.out.println("Bird says \"Hello\"");
+    }
 }
