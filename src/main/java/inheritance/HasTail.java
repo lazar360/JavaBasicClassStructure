cc15a1290e2c63d2e8bd738d0fb274a3d4e21d28
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-11-14 09:17:53 +0100

    Update files

--- src/main/java/inheritance/HasTail.java
+++ src/main/java/inheritance/HasTail.java
@@ -1,2 +1,10 @@
-package inheritance;public interface HasTail {
+package inheritance;
+
+public interface HasTail {//always public abstract
+    public static final int DEFAULT_TAIL_LENGTH2 = 2;
+    int DEFAULT_TAIL_LENGTH = 2;
+
+    int getTailLength();
+    // int getTailLength2(){}; // does not compile because abstract method can not have a body
+
 }
--- src/main/java/inheritance/Herbivore.java
+++ src/main/java/inheritance/Herbivore.java
@@ -1,10 +1,10 @@
 package inheritance;
 
-public interface Herbivor {
+public interface Herbivore {
 
     default void eatPlants(){
         System.out.println("eat plants");
-    };
+    }
 
     // default int getRequiredPantAmount(); // does not compile because the method ahas no body
     // public int getRequiredPlantAmount(){return ;} // does not compile because there is no default keyword
