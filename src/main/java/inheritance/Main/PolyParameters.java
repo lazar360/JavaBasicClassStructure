c2e0f2c624f284ce1702f52b533579c918a34381
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-11-14 09:18:51 +0100

    Update files

--- src/main/java/inheritance/Main/PolyParameters.java
+++ src/main/java/inheritance/Main/PolyParameters.java
@@ -1,2 +1,60 @@
-package inheritance.Main;public class Polyparameters {
+package inheritance.Main;
+
+import inheritance.Animal;
+import inheritance.Cat;
+import inheritance.Husky;
+
+import java.util.Objects;
+
+public class PolyParameters {
+
+    public static void main(String[] args) {
+
+        Husky husky = new Husky(3);
+        husky.setName("Rex");
+        printDetails(husky);
+
+        Rabbit rabbit = new Rabbit();
+        printDetails(rabbit);
+
+        Cat cat = new Cat(8);
+        printDetails(cat);
+
+        Animal rex = createAnimal("Rex", 4);
+        printDetails(rex);
+        Animal kitty = createAnimal("Kitty", 3);
+        printDetails(kitty);
+
+        Animal bunny = createAnimal("Bunny", 2);
+        printDetails(bunny);
+
+    }
+
+    /*public static void printDetails(Husky husky){
+        husky.printDetails();
+    }
+
+    public static void printDetails(Rabbit rabbit){
+        rabbit.printDetails();
+    }*/
+
+    public static void printDetails(Animal animal) {
+        animal.printDetails();
+    }
+
+    public static Animal createAnimal(String name, int age) {
+        if (Objects.equals(name, "Rex")) {
+            Animal animal = new Husky(age);
+            animal.setName(name);
+            return animal;
+        } else if (Objects.equals(name, "Kitty")) {
+            Animal cat = new Cat(7);
+            cat.setName("Kitty");
+            return cat;
+        }
+
+        Animal rabbit= new Rabbit(age);
+        rabbit.setName(name);
+        return rabbit;
+    }
 }
--- src/main/java/inheritance/Main/Polymorphism.java
+++ src/main/java/inheritance/Main/Polymorphism.java
@@ -1,2 +1,37 @@
-package inheritance.Main;public class Polymorphism {
+package inheritance.Main;
+
+import inheritance.*;
+
+public class Polymorphism {
+
+    public static void main(String[] args) {
+
+        Husky husky = new Husky(5);
+
+        husky.setName("Rex");
+
+        Dog dog = husky; // another reference to Husky object in memory
+        dog.printDetails();
+
+        HasTail hasTail = husky;
+        System.out.println("tail length= " + hasTail.getTailLength());
+        System.out.println("tail length husky= " + husky.getTailLength());
+
+        Animal cat = new Cat(2);
+        Cat myCat = (Cat)cat;
+
+        Animal animal = husky;
+        System.out.println("tail length2= " + hasTail.getTailLength());
+        husky.eat();
+
+        husky.printDetails();
+        husky.printName(); // does not compile because method is protected
+
+        // Bear bear = (Bear) "test"; // does not compile because not related in hierarchy
+        // Dog newDog = (Dog) cat; // Casting 'cat' to 'Dog' will produce 'ClassCastException' for any non-null value
+
+        Animal rabbitAnimal = new Rabbit();
+        rabbitAnimal.printDetails();
+
+    }
 }
