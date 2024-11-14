70c055d0e0ab1121a57a1bbad2848ac9a42a5188
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-11-14 09:17:44 +0100

    Update files

--- src/main/java/inheritance/Dog.java
+++ src/main/java/inheritance/Dog.java
@@ -1,2 +1,14 @@
-package inheritance;public class Dog {
+package inheritance;
+
+public abstract class Dog extends Animal implements HasTail{
+    public Dog(int age){
+        super(age);
+        System.out.println("Dog");
+    }
+
+    @Override
+    public void eat() {
+        super.eat();
+        System.out.println("Dog eating");
+    }
 }
--- src/main/java/inheritance/Eagle.java
+++ src/main/java/inheritance/Eagle.java
@@ -1,2 +1,28 @@
-package inheritance;public class Eagle {
+package inheritance;
+
+public class Eagle extends Bird{
+
+    // Overload modifies parameters and body
+    public int fly(int height){
+        System.out.println("Ealgle is flying at "+ height + " meters");
+        return height;
+    }
+
+    // Override modifies body only
+    @Override
+    public void eat(int amount){
+        System.out.println("eagle");
+    }
+
+    // Override can modify accessor for higher
+    @Override
+    public void sayHello(){
+        System.out.println("Bird says Hello");
+    }
+    /*@Override
+    void sayHello(){
+        System.out.println("Bird says \"Hello\"");
+    }*/// does not compile weaker access privilege
+
+
 }
